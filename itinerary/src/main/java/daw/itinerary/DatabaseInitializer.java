3c02460e961fbfc33f461f9c89c3190e5817e89b
RedCeruジュニア <>
2019-02-18 11:28:39 +0100

    Package names changed

--- itinerary/src/main/java/daw/itinerary/DatabaseInitializer.java
+++ itinerary/src/main/java/daw/itinerary/DatabaseInitializer.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package daw.itinerary;
 
 import javax.annotation.PostConstruct;
 
--- itinerary/src/test/java/daw/itinerary/DemoApplicationTests.java
+++ itinerary/src/test/java/daw/itinerary/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package daw.itinerary;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
