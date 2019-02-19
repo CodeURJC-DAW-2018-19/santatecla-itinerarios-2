b3418d86d34a4ba993c016797f4939b0ec7007fb
Rauldilla <38332234+Rauldilla@users.noreply.github.com>
2019-02-19 10:31:58 +0100

    User class added

---
+++ itinerary/src/main/java/daw/itinerary/security/User.java
@@ -0,0 +1,65 @@
+package daw.itinerary.security;
+
+import java.util.List;
+
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+import org.springframework.data.annotation.Id;
+
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	private String name;
+	private String passwordHash;
+
+	@ElementCollection(fetch = FetchType.EAGER)
+	private List<String> roles;
+
+	// Constructor, getters, setters
+
+	public User(String newName, String newPass, String rol) {
+		name = newName;
+		passwordHash = newPass;
+		roles.add(rol);
+	}
+	
+	public User(String newName, String newPass, String rol1, String rol2) {
+		name = newName;
+		passwordHash = newPass;
+		roles.add(rol1);
+		roles.add(rol2);
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getPassword() {
+		return passwordHash;
+	}
+	
+	public List<String> getRoles() {
+		return roles;
+	}
+	
+	public void setName(String newName) {
+		name = newName;
+	}
+	
+	public void setPassword(String newPass) {
+		passwordHash = newPass;
+	}
+	
+	public void setRoles(List<String> newRoles) {
+		roles = newRoles;
+	}
+}
