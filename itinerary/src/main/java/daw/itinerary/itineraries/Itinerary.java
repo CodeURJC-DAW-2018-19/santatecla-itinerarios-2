9f773b6b5cf263f22e60967038cd331369003f8b
RedCeruジュニア <>
2019-02-19 08:49:13 +0100

    Itinerary.java updated
    - Entity annotation added
    - id and name fields added
    - toString method overrided
    - Getters and setters added

--- itinerary/src/main/java/daw/itinerary/itineraries/Itinerary.java
+++ itinerary/src/main/java/daw/itinerary/itineraries/Itinerary.java
@@ -1,4 +1,41 @@
 package daw.itinerary.itineraries;
 
+@Entity
 public class Itinerary {
+
+    /**********************************************
+     * Fields
+     *********************************************/
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String name;
+
+    /**********************************************
+     * Methods
+     *********************************************/
+    @Override
+    public String toString(){
+        return null;
+    }
+
+    /**********************************************
+     * Getters and setters
+     *********************************************/
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
