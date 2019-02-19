7803df61e16c9699710bc50d64bd09a464a26744
GreenCeru <acabezasp7@gmail.com>
2019-02-19 10:31:53 +0100

    Added imports to WebController

--- itinerary/src/main/java/daw/itinerary/web/WebController.java
+++ itinerary/src/main/java/daw/itinerary/web/WebController.java
@@ -1,7 +1,17 @@
 package daw.itinerary.web;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import daw.itinerary.unit.*;
 
 @Controller
 public class WebController {
