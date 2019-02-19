b5fc53872e98e83a0834254e0d81bd760075b39b
Rauldilla <38332234+Rauldilla@users.noreply.github.com>
2019-02-19 10:47:50 +0100

    DatabaseUserLoader added
    New class DatabaseUserLoader created. Now two users are created when you run the application.

---
+++ itinerary/src/main/java/daw/itinerary/security/DatabaseUsersLoader.java
@@ -0,0 +1,22 @@
+package daw.itinerary.security;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseUsersLoader {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@PostConstruct
+	private void initDatabase() {
+		
+		userRepository.save(new User("user", "pass", "ROLE_USER"));
+		
+		userRepository.save(new User("admin", "adminpass", "ROLE_USER", "ROLE_ADMIN"));
+	}
+
+}
