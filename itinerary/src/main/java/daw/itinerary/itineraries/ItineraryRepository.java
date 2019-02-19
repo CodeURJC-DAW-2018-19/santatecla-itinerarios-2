07b849c7c527374e0391bfca69971655c67f808c
RedCeruジュニア <>
2019-02-19 09:25:10 +0100

    Update ItineraryRepository.java

--- itinerary/src/main/java/daw/itinerary/itineraries/ItineraryRepository.java
+++ itinerary/src/main/java/daw/itinerary/itineraries/ItineraryRepository.java
@@ -3,5 +3,5 @@ package daw.itinerary.itineraries;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ItineraryRepository extends JpaRepository<Itinerary, Long> {
-    
+
 }
