b356f1e74407da214115a25c5ab6279ded501bc5
RedCeruジュニア <>
2019-02-19 09:08:23 +0100

    Update Itinerary.java
    Imports that class needs made

--- itinerary/src/main/java/daw/itinerary/itineraries/Itinerary.java
+++ itinerary/src/main/java/daw/itinerary/itineraries/Itinerary.java
@@ -1,5 +1,10 @@
 package daw.itinerary.itineraries;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Entity
 public class Itinerary {
 
