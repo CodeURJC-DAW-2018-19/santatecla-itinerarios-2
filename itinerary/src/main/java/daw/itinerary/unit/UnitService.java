4b2ad49b2d0d50083c01c1e12e4933ef39333427
GreenCeru <acabezasp7@gmail.com>
2019-02-19 10:23:08 +0100

    Create UnitService.java

---
+++ itinerary/src/main/java/daw/itinerary/unit/UnitService.java
@@ -0,0 +1,33 @@
+package daw.itinerary.unit;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/* This service will be used to include features to controller(s)
+ */
+
+@Service
+public class UnitService {
+
+	@Autowired
+	private UnitRepository repository;
+
+	public Optional<Unit> findOne(long id) {
+		return repository.findById(id);
+	}
+
+	public List<Unit> findAll() {
+		return repository.findAll();
+	}
+
+	public void save(Unit unit) {
+		repository.save(unit);
+	}
+
+	public void delete(long id) {
+		repository.deleteById(id);
+	}
+}
