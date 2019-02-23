8d76e92d0f1969daabdb5b1f5478d164043ca50e
IgorT142 <igort142@gmail.com>
2019-02-23 20:30:32 +0100

    File changes and default view
    Now the navigation bar is sticky on the top and by default it shows units

--- itinerary/src/main/java/daw/itinerary/web/WebController.java
+++ itinerary/src/main/java/daw/itinerary/web/WebController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import daw.itinerary.unit.*;
+import daw.itinerary.content.*;
 import daw.itinerary.itineraries.*;
 
 @Controller
@@ -22,9 +23,12 @@ public class WebController {
 	@Autowired
     private UnitService service;
 	
+	@Autowired
+	private ContentService contentService;
+	
 	@RequestMapping("/")
 	public String index () {
-		return "index";
+		return "units";
 	}
 
 }
