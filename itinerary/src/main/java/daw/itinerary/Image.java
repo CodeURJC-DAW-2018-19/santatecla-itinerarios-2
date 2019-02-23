3092cf05ab7e1133e59543cc2108adeaf3e9534c
IgorT142 <igort142@gmail.com>
2019-02-23 17:47:41 +0100

    Image class added
    This class will serve to attach images to
    files

---
+++ itinerary/src/main/java/daw/itinerary/Image.java
@@ -0,0 +1,29 @@
+package daw.itinerary;
+
+public class Image {
+
+	private int id;
+	private String name;
+
+	public void Image(int id, String name) {
+		this.name = name;
+		this.id = id;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
