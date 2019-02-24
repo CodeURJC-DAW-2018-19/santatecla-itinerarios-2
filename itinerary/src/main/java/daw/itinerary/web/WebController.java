659b49b083f1ec367977bb71adc3cea7720d77d6
Rauldilla <38332234+Rauldilla@users.noreply.github.com>
2019-02-24 16:58:55 +0100

    Update WebController
    Some changes  to WebController, like addUserToModel method.

--- itinerary/src/main/java/daw/itinerary/web/WebController.java
+++ itinerary/src/main/java/daw/itinerary/web/WebController.java
@@ -1,51 +1,55 @@
 package daw.itinerary.web;
 
-import java.util.Optional;
-
-import javax.annotation.PostConstruct;
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import daw.itinerary.unit.*;
+import daw.itinerary.user.UserComponent;
 import daw.itinerary.content.*;
-import daw.itinerary.itineraries.*;
 
 @Controller
 public class WebController {
 
 	@Autowired
-    private UnitService service;
-	
+	private UnitService service;
+
+	@Autowired
+	private UserComponent userComponent;
+
 	@Autowired
 	private ContentService contentService;
-	
+
+	@ModelAttribute
+	public void addUserToModel(Model model) {
+		boolean logged = userComponent.getLoggedUser() != null;
+		model.addAttribute("logged", logged);
+		if (logged) {
+			model.addAttribute("admin", userComponent.getLoggedUser().getRoles().contains("ROLE_ADMIN"));
+			model.addAttribute("userName", userComponent.getLoggedUser().getName());
+		}
+	}
+
 	@RequestMapping("/")
-	public String index () {
+	public String index() {
 		return "units";
 	}
+
 	@RequestMapping("/itinerary")
 	public String itinerary() {
 		return "itinerary";
 	}
 
 	@RequestMapping("/login")
-	public String login(Model model, HttpServletRequest request) {
-		 
-		 CsrfToken token = (CsrfToken) request.getAttribute("_csrf");
-		 model.addAttribute("token", token.getToken());
+	public String login(Model model) {
 
-		 return "login";
+		model.addAttribute("hideLogin", true);
+		return "login";
 	}
-	
+
 	@RequestMapping("/units")
 	public String units() {
 		return "units";
@@ -53,8 +57,8 @@ public class WebController {
 
 	@GetMapping("/contents")
 	public String contents(Model model) {
-		
-		model.addAttribute("content",contentService.findAll());
+
+		model.addAttribute("content", contentService.findAll());
 		return "contents";
 	}
 }
