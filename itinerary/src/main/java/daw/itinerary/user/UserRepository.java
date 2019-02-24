f8ccd035c95a698867a1f31fd8c5d2379ba4336c
Rauldilla <38332234+Rauldilla@users.noreply.github.com>
2019-02-24 12:22:42 +0100

    SMALL CHANGE AT UserRepository

--- itinerary/src/main/java/daw/itinerary/user/UserRepository.java
+++ itinerary/src/main/java/daw/itinerary/user/UserRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-	List<User> findByNameContaining(String name);
+	User findByName(String name);
 }
