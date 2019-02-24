21cfa3a41fb8afd57dcb56f97e0a84f5aac6ad7b
Rauldilla <38332234+Rauldilla@users.noreply.github.com>
2019-02-24 16:13:42 +0100

    LOGINCONTROLLER ADDED

---
+++ itinerary/src/main/java/daw/itinerary/security/LoginController.java
@@ -0,0 +1,50 @@
+package daw.itinerary.security;
+
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import daw.itinerary.user.User;
+import daw.itinerary.user.UserComponent;
+
+@Controller
+public class LoginController {
+
+	private static final Logger log = LoggerFactory.getLogger(LoginController.class);
+
+	@Autowired
+	private UserComponent userComponent;
+
+	@RequestMapping("/logIn")
+	public ResponseEntity<User> logIn() {
+
+		if (!userComponent.isLoggedUser()) {
+			log.info("No user logged");
+			return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+		} else {
+			User loggedUser = userComponent.getLoggedUser();
+			log.info("Logged as " + loggedUser.getName());
+			return new ResponseEntity<>(HttpStatus.OK);
+		}
+	}
+
+	@RequestMapping("/logOut")
+	public ResponseEntity<Boolean> logOut(HttpSession session) {
+
+		if (!userComponent.isLoggedUser()) {
+			log.info("No user logged");
+			return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+		} else {
+			session.invalidate();
+			log.info("Logged Out");
+			return new ResponseEntity<>(true, HttpStatus.OK);
+		}
+	}
+
+}
