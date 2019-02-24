339e6c1d2afe0dbd913a509274d7b0338554ea69
IgorT142 <igort142@gmail.com>
2019-02-24 18:30:15 +0100

    PostMapping import fixed

--- itinerary/src/main/java/daw/itinerary/web/WebController.java
+++ itinerary/src/main/java/daw/itinerary/web/WebController.java
@@ -25,6 +25,7 @@ import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
