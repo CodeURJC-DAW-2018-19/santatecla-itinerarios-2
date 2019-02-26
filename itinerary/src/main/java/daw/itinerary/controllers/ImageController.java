9f452f11ec439f9da4f114e909cbe495a26697b8
Igort142 <38567109+IgorT142@users.noreply.github.com>
2019-02-26 10:01:55 +0100

    WebController splitted
    Moved Image controller functions to ImageController

---
+++ itinerary/src/main/java/daw/itinerary/controllers/ImageController.java
@@ -0,0 +1,102 @@
+package daw.itinerary.controllers;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import daw.itinerary.content.ContentService;
+import daw.itinerary.content.Image;
+
+public class ImageController {
+	
+	@Autowired
+	private ContentService contentService;
+
+	/* Image uploading */
+
+	private static final Path FILES_FOLDER = Paths.get(System.getProperty("user.dir"), "images");
+
+	private Map<Integer, Image> images = new ConcurrentHashMap<>();
+	private AtomicInteger imageId = new AtomicInteger();
+
+	@PostConstruct
+	public void init() throws IOException {
+
+		if (!Files.exists(FILES_FOLDER)) {
+			Files.createDirectories(FILES_FOLDER);
+		}
+	}
+
+	@PostMapping(value = "/contents/upload")
+	public String imageUploaded(Model model, @RequestParam("file") MultipartFile file) {
+		
+		int id = imageId.getAndIncrement();
+
+		String fileName = "image-" + id + ".jpg";
+
+		if (!file.isEmpty()) {
+			try {
+				File uploadedFile = new File(FILES_FOLDER.toFile(), fileName);
+				file.transferTo(uploadedFile);
+
+				images.put(id, new Image(id, fileName));
+				return "upload";
+			} catch (Exception e) {
+				model.addAttribute("error", e.getClass().getName() + ":" + e.getMessage());
+
+				return "upload";
+			}
+		} else {
+			model.addAttribute("error", "The file is empty");
+			return "upload";
+		}
+
+	}
+
+	@GetMapping("/images/{id}")
+	public void fileDownload(@PathVariable String id, HttpServletResponse res)
+			throws FileNotFoundException, IOException {
+		
+		String fileName = "image-" + id + ".jpg";
+
+		Path image = FILES_FOLDER.resolve(fileName);
+		
+		if(Files.exists(image)) {
+			res.setContentType("image/jpeg");
+			res.setContentLength((int) image.toFile().length());
+			FileCopyUtils.copy(Files.newInputStream(image), res.getOutputStream());
+			
+		} else {
+			res.sendError(401, "File does not exist");
+		}
+
+	}
+	
+	@GetMapping("/contents")
+	public String contents(Model model) {
+
+		model.addAttribute("content", contentService.findAll());
+
+		model.addAttribute("images", images.values());
+
+		return "contents";
+	}
+}
