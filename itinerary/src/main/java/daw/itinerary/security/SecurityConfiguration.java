7deb68b0a22cfb7e9155d66c827e11e93a4f5ce2
IgorT142 <igort142@gmail.com>
2019-02-27 21:34:50 +0100

    Revert "Merge branch 'master' into IgorBranch"
    This reverts commit f5414bbecfac06d675272db48027c8b97ca3612e, reversing
    changes made to 6b313d7257cea88737a4f2fac351b88350db8a57.

--- itinerary/src/main/java/daw/itinerary/security/SecurityConfiguration.java
+++ itinerary/src/main/java/daw/itinerary/security/SecurityConfiguration.java
@@ -1,40 +1,19 @@
 package daw.itinerary.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+	protected void configure (HttpSecurity http) throws Exception {
+		
+		//Public pages
+		http.authorizeRequests().antMatchers("/").permitAll().and().csrf().disable();
 
-	@Autowired
-	public UserRepositoryAuthProvider userRepoAuthProvider;
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-
-		// Public pages
-		http.authorizeRequests().anyRequest().permitAll();
-		http.authorizeRequests().antMatchers("/login").permitAll();
-		http.authorizeRequests().antMatchers("/css/**", "/js/**", "/material-dashboard-html-v2.1.1/**").permitAll();
-
-		// Private pages (all other pages)
-
-		// Login form
-		http.formLogin().loginPage("/login");
-		http.formLogin().usernameParameter("username");
-		http.formLogin().passwordParameter("password");
-		http.formLogin().defaultSuccessUrl("/");
-		http.formLogin().failureUrl("/loginerror");
-
-	}
-
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		// Database authentication provider
-		auth.authenticationProvider(userRepoAuthProvider);
+		
+		//Private pages
 	}
 
 }
