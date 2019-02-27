cbc4f461b7c8a7f0cef894b24e59100a82309591
Rauldilla <38332234+Rauldilla@users.noreply.github.com>
2019-02-27 21:21:27 +0100

    Revert "Revert "Revert "Merge branch 'paulBranch'"""
    This reverts commit 7b9c37bef34f33b53243e4f0ed49625f54e37175.

--- itinerary/src/main/java/daw/itinerary/user/UserRepository.java
+++ itinerary/src/main/java/daw/itinerary/user/UserRepository.java
@@ -1,11 +1,9 @@
 package daw.itinerary.user;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-	List<User> findByNameContaining(String name);
+	User findByName(String name);
 }
