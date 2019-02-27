b92097d38afc8e29359e9dda75fe6d825862571e
IgorT142 <igort142@gmail.com>
2019-02-27 21:49:24 +0100

    Revert "Revert "Revert "Merge branch 'master' into IgorBranch"""
    This reverts commit a7a2b170937e3680b4ea6bb194e426aecb718f20.

--- itinerary/src/main/java/daw/itinerary/user/UserRepository.java
+++ itinerary/src/main/java/daw/itinerary/user/UserRepository.java
@@ -1,9 +1,11 @@
 package daw.itinerary.user;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-	User findByName(String name);
+	List<User> findByNameContaining(String name);
 }
