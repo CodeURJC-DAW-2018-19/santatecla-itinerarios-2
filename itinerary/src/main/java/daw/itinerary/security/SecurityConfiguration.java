d64f9f69c4d002599ec83d0aacad01b185d7102c
Rauldilla <38332234+Rauldilla@users.noreply.github.com>
2019-02-27 20:18:25 +0100

    Everything public
    SecurityConfiguration changed in order to make every html public, and to make easy to work while the LOGIN doesn't work

--- itinerary/src/main/java/daw/itinerary/security/SecurityConfiguration.java
+++ itinerary/src/main/java/daw/itinerary/security/SecurityConfiguration.java
@@ -6,39 +6,35 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-	
+
 	@Autowired
-    public UserRepositoryAuthProvider userRepoAuthProvider;
-	
+	public UserRepositoryAuthProvider userRepoAuthProvider;
+
 	@Override
-	protected void configure (HttpSecurity http) throws Exception {
-		
-		//Public pages
-		http.authorizeRequests().antMatchers("/static/**", "/").permitAll();
-        http.authorizeRequests().antMatchers("/login").permitAll();
-        http.authorizeRequests().antMatchers("/css/**", "/js/**", "/material-dashboard-html-v2.1.1/**").permitAll();
-        
-        // Private pages (all other pages)
-        http.authorizeRequests().anyRequest().authenticated();
+	protected void configure(HttpSecurity http) throws Exception {
 
-        //Login form
-        http.formLogin().loginPage("/login");
-        http.formLogin().usernameParameter("username");
-        http.formLogin().passwordParameter("password");
-        http.formLogin().defaultSuccessUrl("/");
-        http.formLogin().failureUrl("/loginerror");
+		// Public pages
+		http.authorizeRequests().anyRequest().permitAll();
+		http.authorizeRequests().antMatchers("/login").permitAll();
+		http.authorizeRequests().antMatchers("/css/**", "/js/**", "/material-dashboard-html-v2.1.1/**").permitAll();
 
-		
+		// Private pages (all other pages)
+
+		// Login form
+		http.formLogin().loginPage("/login");
+		http.formLogin().usernameParameter("username");
+		http.formLogin().passwordParameter("password");
+		http.formLogin().defaultSuccessUrl("/");
+		http.formLogin().failureUrl("/loginerror");
+
 	}
-	
+
 	@Override
-    protected void configure(AuthenticationManagerBuilder auth)
-            throws Exception {
-        // Database authentication provider
-        auth.authenticationProvider(userRepoAuthProvider);
-    }
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// Database authentication provider
+		auth.authenticationProvider(userRepoAuthProvider);
+	}
 
 }
