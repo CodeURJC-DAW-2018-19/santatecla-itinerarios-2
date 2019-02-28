3212e806bb33ae7f598a5e603e142a67e702eec1
RamsayBoy <38114695+RamsayBoy@users.noreply.github.com>
2019-02-28 21:08:00 +0100

    Add PathVariable id
    PathVariable "units_id" for adding contents to contents.html template and show them

--- itinerary/src/main/java/daw/itinerary/controllers/ContentController.java
+++ itinerary/src/main/java/daw/itinerary/controllers/ContentController.java
@@ -1,9 +1,12 @@
 package daw.itinerary.controllers;
 
+import daw.itinerary.content.Content;
+import daw.itinerary.content.ContentService;
 import daw.itinerary.unit.UnitService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,9 +16,10 @@ public class ContentController
     private UnitService unitService;
 
     @RequestMapping("/units/{units_id}/contents")
-    public String contents(Model model)
+    public String contents(Model model, @PathVariable long units_id)
     {
         model.addAttribute("unit", unitService.findAll());
+        model.addAttribute("contents", unitService.findOne(units_id).get().getContents());
         return "contents";
     }
 }
