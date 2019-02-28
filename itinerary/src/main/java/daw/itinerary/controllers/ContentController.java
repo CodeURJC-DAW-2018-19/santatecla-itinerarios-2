19f28821e9bc3b003a73ffb7dadd012e3fda0552
RamsayBoy <38114695+RamsayBoy@users.noreply.github.com>
2019-02-28 23:17:20 +0100

    Contents shown for each unit

--- itinerary/src/main/java/daw/itinerary/controllers/ContentController.java
+++ itinerary/src/main/java/daw/itinerary/controllers/ContentController.java
@@ -2,6 +2,7 @@ package daw.itinerary.controllers;
 
 import daw.itinerary.content.Content;
 import daw.itinerary.content.ContentService;
+import daw.itinerary.unit.Unit;
 import daw.itinerary.unit.UnitService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,11 +16,13 @@ public class ContentController
     @Autowired
     private UnitService unitService;
 
-    @RequestMapping("/units/{units_id}/contents")
-    public String contents(Model model, @PathVariable long units_id)
+    @RequestMapping("/units/{id}/contents")
+    public String contents(Model model, @PathVariable long id)
     {
+        Unit unit = unitService.findOne(id).get();
+        model.addAttribute("units", unit);
         model.addAttribute("unit", unitService.findAll());
-        model.addAttribute("contents", unitService.findOne(units_id).get().getContents());
+        model.addAttribute("contents", unit.getContents());
         return "contents";
     }
 }
