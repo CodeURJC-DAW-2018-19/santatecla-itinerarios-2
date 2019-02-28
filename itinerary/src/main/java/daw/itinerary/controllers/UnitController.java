32155bcf28e2da0b96c8489448db390dd0dfbbd8
RamsayBoy <38114695+RamsayBoy@users.noreply.github.com>
2019-02-28 13:20:56 +0100

    Create UnitController.java
    This class contains the requests mapping which were placed on WebController.

---
+++ itinerary/src/main/java/daw/itinerary/controllers/UnitController.java
@@ -0,0 +1,39 @@
+package daw.itinerary.controllers;
+
+import daw.itinerary.unit.UnitService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UnitController
+{
+    @Autowired
+    private UnitService unitService;
+
+    @RequestMapping("/")
+    public String index()
+    {
+
+        return "redirect:units";
+    }
+
+    @RequestMapping("/units")
+    public String units(Model model)
+    {
+
+        model.addAttribute("unit", unitService.findAll());
+
+        return "units";
+    }
+
+    @RequestMapping("/units/{units_id}/itinerary")
+    public String itinerary(Model model)
+    {
+        model.addAttribute("unit", unitService.findAll());
+//		model.addAttribute("unit", unitService.findOne(id));
+//		model.addAttribute("itinerary", itService.findAll());
+        return "itinerary";
+    }
+}
