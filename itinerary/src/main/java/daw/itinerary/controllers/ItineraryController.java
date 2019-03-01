177dd6861bf527f581e8f4da43e6bb391ac00b2c
RamsayBoy <38114695+RamsayBoy@users.noreply.github.com>
2019-03-01 22:35:08 +0100

    New request mapping
    New request mapping for itinerary controller that shows tabs for each unit itinerary

--- itinerary/src/main/java/daw/itinerary/controllers/ItineraryController.java
+++ itinerary/src/main/java/daw/itinerary/controllers/ItineraryController.java
@@ -1,9 +1,12 @@
 package daw.itinerary.controllers;
 
+import daw.itinerary.itineraries.ItineraryService;
+import daw.itinerary.unit.Unit;
 import daw.itinerary.unit.UnitService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,18 +15,23 @@ public class ItineraryController
     @Autowired
     private UnitService unitService;
 
+    @Autowired
+    private ItineraryService itineraryService;
+
     @RequestMapping("/itinerary")
     public String itinerary()
     {
         return "itinerary";
     }
 
-    @RequestMapping("/units/{units_id}/itinerary")
-    public String itinerary(Model model)
+    @RequestMapping("/units/{unit_id}/itinerary/{id}")
+    public String itinerary(Model model, @PathVariable long unit_id)
     {
+        Unit unit = unitService.findOne(unit_id).get();
+        model.addAttribute("units", unit);
         model.addAttribute("unit", unitService.findAll());
-//		model.addAttribute("unit", unitService.findOne(id));
-//		model.addAttribute("itinerary", itService.findAll());
+        model.addAttribute("itineraries", unit.getContents());
+
         return "itinerary";
     }
 }
