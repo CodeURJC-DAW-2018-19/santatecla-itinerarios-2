80628b20ca81b40bb0576037600121ac3372d6d1
RamsayBoy <38114695+RamsayBoy@users.noreply.github.com>
2019-03-02 22:07:43 +0100

    Contents pageabled

--- itinerary/src/main/java/daw/itinerary/controllers/ContentController.java
+++ itinerary/src/main/java/daw/itinerary/controllers/ContentController.java
@@ -1,10 +1,15 @@
 package daw.itinerary.controllers;
 
+import daw.itinerary.content.Content;
+import daw.itinerary.content.ContentService;
 import daw.itinerary.unit.Unit;
 import daw.itinerary.unit.UnitService;
 import daw.itinerary.user.UserComponent;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,15 +20,22 @@ public class ContentController
 {
     @Autowired
     private UnitService unitService;
-    
+
+    @Autowired
+    private ContentService contentService;
+
     @Autowired
 	private UserComponent userComponent;
 
     @RequestMapping("/units/{id}/contents")
-    public String contents(Model model, @PathVariable long id)
+    public String contents(Model model, @PathVariable long id, Pageable page)
     {
     	boolean logged = userComponent.getLoggedUser() != null;
 		model.addAttribute("logged", logged);
+
+        Page<Content> contents = contentService.findAll(new PageRequest(0, 10));
+        model.addAttribute("contentsPage", contents);
+
         Unit unit = unitService.findOne(id).get();
         model.addAttribute("units", unit);
         model.addAttribute("unit", unitService.findAll());
