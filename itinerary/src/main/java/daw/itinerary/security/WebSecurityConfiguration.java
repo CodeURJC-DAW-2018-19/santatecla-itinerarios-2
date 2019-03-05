2aecde03f6d1a320f2b0377391a9ac3ff67ea41b
Rauldilla <38332234+Rauldilla@users.noreply.github.com>
2019-03-05 10:10:45 +0100

    Update WebSecurityConfiguration.java
    SecurityConfiguration name refactored to WebSecurityConfiguration

--- itinerary/src/main/java/daw/itinerary/security/WebSecurityConfiguration.java
+++ itinerary/src/main/java/daw/itinerary/security/WebSecurityConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	public UserRepositoryAuthProvider userRepoAuthProvider;
