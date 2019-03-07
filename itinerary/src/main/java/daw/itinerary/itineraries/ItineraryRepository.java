c381de6e7d5ee5ae764701f46813f95cdaa7a34a
RamsayBoy <38114695+RamsayBoy@users.noreply.github.com>
2019-03-07 21:14:29 +0100

    New searching methods

--- itinerary/src/main/java/daw/itinerary/itineraries/ItineraryRepository.java
+++ itinerary/src/main/java/daw/itinerary/itineraries/ItineraryRepository.java
@@ -1,7 +1,13 @@
 package daw.itinerary.itineraries;
 
+import daw.itinerary.unit.Unit;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Set;
 
 public interface ItineraryRepository extends JpaRepository<Itinerary, Long> {
 
+    // Lo busca shenron
+    Itinerary findItineraryByIdAndUnitId(long itId, long unId);
+    Set<Itinerary> findAllByUnitId(long id);
 }
