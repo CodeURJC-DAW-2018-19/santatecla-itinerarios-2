23ea533954c691fa8af15ba7b67b4d93c15855a7
RamsayBoy <38114695+RamsayBoy@users.noreply.github.com>
2019-03-07 21:11:48 +0100

    Add rest methods
    Methods added:
    - /api/itinerary
    - /api/units/{unit_id}/itinerary/{id}
    - /api/units/{unit_id}/itinerary
    - /api/units/{unit_id}/newItinerary

--- itinerary/src/main/java/daw/itinerary/api/ItineraryRestController.java
+++ itinerary/src/main/java/daw/itinerary/api/ItineraryRestController.java
@@ -1,21 +1,30 @@
 package daw.itinerary.api;
 
 import daw.itinerary.content.ContentService;
+import daw.itinerary.itineraries.Itinerary;
+import daw.itinerary.itineraries.ItineraryRepository;
+import daw.itinerary.itineraries.ItineraryService;
 import daw.itinerary.unit.Unit;
 import daw.itinerary.unit.UnitService;
 import daw.itinerary.user.UserComponent;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-/*
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Set;
+
 @RestController
 public class ItineraryRestController
 {
     @Autowired
     private UnitService unitService;
+
+    @Autowired
+    private ItineraryService itineraryService;
     
     @Autowired
 	private UserComponent userComponent;
@@ -23,6 +32,38 @@ public class ItineraryRestController
     @Autowired
     private ContentService contentService;
 
+    @Autowired
+    private ItineraryRepository repoItinerary;
+
+    @RequestMapping("/api/itinerary")
+    public List<Itinerary> itinerary()
+    {
+        return repoItinerary.findAll();
+    }
+
+    @RequestMapping("/api/units/{unit_id}/itinerary/{id}")
+    public Itinerary itinerary(Model model, @PathVariable long unit_id, @PathVariable long id)
+    {
+        return repoItinerary.findItineraryByIdAndUnitId(id, unit_id);
+    }
+
+    @RequestMapping("/api/units/{unit_id}/itinerary")
+    public Set<Itinerary> itinerary(Model model, @PathVariable long unit_id)
+    {
+        return repoItinerary.findAllByUnitId(unit_id);
+    }
+
+    @PostMapping("/api/units/{unit_id}/newItinerary")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Itinerary addItinerary(@RequestBody Itinerary itinerary, @PathVariable long unit_id)
+    {
+        itinerary.setUnit(unitService.findOne(unit_id).get());
+        itineraryService.save(itinerary);
+
+        return itinerary;
+    }
+
+    /*
     @RequestMapping("/itinerary")
     public String itinerary()
     {
@@ -41,5 +82,5 @@ public class ItineraryRestController
         model.addAttribute("itineraries", unit.getItineraries());
 
         return "itinerary";
-    }
+    }*/
 }
