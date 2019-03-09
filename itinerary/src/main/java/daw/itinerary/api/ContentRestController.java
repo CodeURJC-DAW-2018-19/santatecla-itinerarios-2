9bfbd369518b7f77f3adc7d35bba9215088f561d
Igort142 <38567109+IgorT142@users.noreply.github.com>
2019-03-09 21:12:05 +0100

    Updated ContentRestController
    Refactored the old method and added the setHasImage = true when you upload an image.

--- itinerary/src/main/java/daw/itinerary/api/ContentRestController.java
+++ itinerary/src/main/java/daw/itinerary/api/ContentRestController.java
@@ -43,13 +43,11 @@ public class ContentRestController {
 	/* Image "downloading" */
 	private static final Path FILES_FOLDER = Paths.get(System.getProperty("user.dir"), "images");
 
-    @RequestMapping("/api/units/{id}/contents")
-    @ResponseStatus(HttpStatus.OK)
-    public Page<Content> unitContents(Model model, @PathVariable long id, @PageableDefault(size = 10) Pageable page)
-    {
-        return repo.findAllByUnitId(id, page);
-    }
-    
+	@RequestMapping("/api/units/{id}/contents")
+	@ResponseStatus(HttpStatus.OK)
+	public Page<Content> unitContents(Model model, @PathVariable long id, @PageableDefault(size = 10) Pageable page) {
+		return repo.findAllByUnitId(id, page);
+	}
 
 	@GetMapping("/api/contents")
 	public Collection<Content> getAllContents() {
@@ -65,44 +63,42 @@ public class ContentRestController {
 		return content;
 	}
 
-	@PostMapping("/api/units/{id}/contents/{content_id}/uploadImage")
-	public Content uploadImageToContent(@PathVariable("id") long id, @PathVariable("content_id") long content_id,
-			@RequestParam("file") MultipartFile file) throws IllegalStateException, IOException {
-		if (!file.isEmpty()) {
-			String fileName = "image-" + content_id + ".jpg";
-			File uploadedFile = new File(FILES_FOLDER.toFile(), fileName);
-			file.transferTo(uploadedFile);
-//			contentService.findOne(content_id).get().setImage(fileName);
-		}
-		return contentService.findOne(content_id).get();
-	}
+	// This method was used when images where stored in file system
+	/*
+	 * @PostMapping("/api/units/{id}/contents/{content_id}/uploadImage") public
+	 * Content uploadImageToContent(@PathVariable("id") long
+	 * id, @PathVariable("content_id") long content_id,
+	 * 
+	 * @RequestParam("file") MultipartFile file) throws IllegalStateException,
+	 * IOException { if (!file.isEmpty()) { String fileName = "image-" + content_id
+	 * + ".jpg"; File uploadedFile = new File(FILES_FOLDER.toFile(), fileName);
+	 * file.transferTo(uploadedFile); //
+	 * contentService.findOne(content_id).get().setImage(fileName); } return
+	 * contentService.findOne(content_id).get(); }
+	 */
 
 	@DeleteMapping("/api/units/{id}/deleteContent")
 	public Content deleteContent(@PathVariable("id") long id, @RequestParam("content_id") long content_id) {
 		Content content = contentService.findOne(content_id).get();
 		if (contentService.findOne(content_id).isPresent()) {
-			
+
 			contentService.delete(content_id);
 			return content;
 		}
 
 		return content;
 	}
-	
-	@PostMapping("/api/units/{id}/contents/{content_id}/uploadImageRaw")
+
+	@PostMapping("/api/units/{id}/contents/{content_id}/uploadImage")
 	public Content uploadImageToContentArray(@PathVariable("id") long id, @PathVariable("content_id") long content_id,
 			@RequestParam("file") MultipartFile file) throws IllegalStateException, IOException {
 		if (!file.isEmpty()) {
-            FileInputStream inputStream = (FileInputStream) file.getInputStream();
-            byte[] bytes = file.getBytes();
-            contentService.findOne(content_id).get().setImageRaw(bytes);
-			String fileName = "image-" + content_id + ".jpg";
-			File uploadedFile = new File(FILES_FOLDER.toFile(), fileName);
-			file.transferTo(uploadedFile);
-//			contentService.findOne(content_id).get().setImage(fileName);
+			FileInputStream inputStream = (FileInputStream) file.getInputStream();
+			byte[] bytes = file.getBytes();
+			contentService.findOne(content_id).get().setImageRaw(bytes);
+			contentService.findOne(content_id).get().setHasImage(true);
 		}
 		return contentService.findOne(content_id).get();
 	}
-	
 
 }
