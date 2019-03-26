90913581e14942be4279ba70ca9e8268cb5ed37d
Fibo-kun <acabezasp7@gmail.com>
2019-03-26 09:40:52 +0100

    Delete angular

--- angular/src/app/unit.service.ts
+++
@@ -1,5 +0,0 @@
-import { Injectable } from '@angular/core';
-import { Http, Headers, RequestOptions } from '@angular/http';
-import { Observable } from 'rxjs';
-import { map, catchError } from 'rxjs/operators';
-
