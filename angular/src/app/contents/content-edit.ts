2f3c2f391249817c7964ec674c45d6b53877f50e
Igort142 <38567109+IgorT142@users.noreply.github.com>
2019-04-04 01:34:41 +0200

    Updated edit contents
    Now you see the image aswell when you're editing a content

--- angular/src/app/contents/content-edit.ts
+++ angular/src/app/contents/content-edit.ts
@@ -7,16 +7,20 @@ import {LoginService} from "../login/login.service";
     template: `
   <div *ngIf="content">
   <h2> Editar contenido "{{content.title}}"</h2>
+      <div *ngIf="content.hasImage" style="float: right; margin-right: 12.5%">
+          <img src="data:image/JPEG;base64,{{content.imageRaw}}">
+      </div>
       <form class="example-form" style="display: flex; flex-direction: column; width: 50%" >
           <mat-form-field>
-              <input matInput placeholder="Titulo" value="{{content.title}}">
+              <input matInput placeholder="Título" value="{{content.title}}">
           </mat-form-field>
           <mat-form-field class="example-full-width">
-              <textarea matInput placeholder="Descripcion" value="{{content.desc}}" style="height: 5em"></textarea>
+              <textarea matInput placeholder="Descripción" value="{{content.desc}}" style="height: 5em"></textarea>
           </mat-form-field>
       </form>
   </div>
-    <button mat-raised-button color="warn">Guardar</button>`
+    <button mat-raised-button color="warn">Guardar</button>
+`
 })
 
 export class ContentEdit {
